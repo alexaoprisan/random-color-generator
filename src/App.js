c19aa4a67b1af0d4dd8046ac5f8266419a7afc4e
Johanna-Maria-Alexandra Oprisan <alexaoprisan@gmail.com>
2024-01-18 16:03:37 +0100

    Fixed broken URL after generating meme.

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import React, { useState } from 'react';
 // Defining the MemeForm component
 function MemeForm() {
   // Setting up state variables for topText, bottomText, and selectedTemplate
-  const [topText, setTopText] = useState('');
-  const [bottomText, setBottomText] = useState('');
-  const [selectedTemplate, setSelectedTemplate] = useState('buzz');
-  // const defaultPic =
-  // const memePic =
+  const [topText, setTopText] = useState('_');
+  const [bottomText, setBottomText] = useState('_');
+  const [selectedTemplate, setSelectedTemplate] = useState('oprah');
+  const defaultPic = `https://api.memegen.link/images/${selectedTemplate}/_/_.png`;
+  const generatedMeme = `https://api.memegen.link/images/${selectedTemplate}/${topText}/${bottomText}.png`;
 
   // Event handler to update the states based on user input
   function handleTopTextChange(event) {
@@ -27,9 +27,7 @@ function MemeForm() {
   const handleClick = () => {
     // Invoking the saveAs function with the meme URL to trigger the download
     saveAs(
-      `https://api.memegen.link/images/${selectedTemplate}/${encodeURIComponent(
-        topText,
-      )}/${encodeURIComponent(bottomText)}.jpg`,
+      `https://api.memegen.link/images/${selectedTemplate}/${topText}/${bottomText}.png`,
     );
   };
 
@@ -40,7 +38,6 @@ function MemeForm() {
       <label htmlFor="top-text">Top text</label>
       <input
         id="top-text"
-        value={topText}
         onChange={handleTopTextChange}
         placeholder="Enter top text"
       />
@@ -48,7 +45,6 @@ function MemeForm() {
       <label htmlFor="bottom-text">Bottom text</label>
       <input
         id="bottom-text"
-        value={bottomText}
         onChange={handleBottomTextChange}
         placeholder="Enter bottom text"
       />
@@ -56,20 +52,22 @@ function MemeForm() {
       <label htmlFor="meme-template">Meme template</label>
       <input
         id="meme-template"
-        value={selectedTemplate}
         onChange={handleTemplateChange}
         placeholder="Enter meme template"
       />
 
-      {/* Image element displaying the generated meme */}
-      <img
-        id="meme-image"
-        data-test-id="meme-image"
-        src={`https://api.memegen.link/images/${selectedTemplate}/${encodeURIComponent(
-          topText,
-        )}/${encodeURIComponent(bottomText)}.jpg`}
-        alt="Generated Meme"
-      />
+      {/* conditional Image container which shows different picture depending on the state of upper text and meme name. If one of them is empty URL breaks. */}
+      <div id="meme-image">
+        {topText === '' || selectedTemplate === '' ? (
+          <img src={defaultPic} data-test-id="meme-image" alt="default Meme" />
+        ) : (
+          <img
+            src={generatedMeme}
+            data-test-id="meme-image"
+            alt="Generated Meme"
+          />
+        )}
+      </div>
 
       {/* Button to trigger the download */}
       <div className="App">
